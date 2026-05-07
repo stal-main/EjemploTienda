00e783fee401411585de358ab00a1823fb27cf59
Kevin Solano <7stalyakov7@gmail.com>
2026-05-07 13:59:13 -0600

    Cliente

--- src/logica/Cliente.java
+++ src/logica/Cliente.java
@@ -1,5 +1,35 @@
 package logica;
 
 public class Cliente {
+	
+	private String id;
+	
+	private String nombre;
+	
+	private String email;
+	
+	public Cliente(String id, String nombre, String email) {
+		
+		this.id = id;
+		
+		this.nombre = nombre;
+		
+		this.email = email;
+	}
+	
+	public String getId() {
+		
+		return id;
+	}
+	
+	public String getNombre() {
+		
+		return nombre;
+	}
 
+	public String getEmail() {
+	
+		return email;
+	}	
+ 
 }
