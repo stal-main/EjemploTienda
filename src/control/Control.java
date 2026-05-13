fc40f5ac4abb2cffd1ca9ea9983748417d186d0e
Kevin Solano <7stalyakov7@gmail.com>
2026-05-13 14:17:28 -0600

    Controladora

--- src/control/Control.java
+++ src/control/Control.java
@@ -1,5 +1,52 @@
 package control;
 
-public class Control {
+import logica.Client;
+import logica.Order;
+import logica.Products;
 
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Control {
+	
+	private Map<String, Client> clients;
+	private Map<Integer, Products> products;
+	private Map<Integer, Order> orders;
+	
+	public Control() {
+		
+		clients = new HashMap<>();
+		
+		products = new HashMap<>();
+		
+		orders = new HashMap<>();
+		
+	}
+	
+	public Map<String, String> getClients() {
+		
+		Map<String, String> result = new HashMap<>();
+		
+		for (Client c : clients.values()) {
+			
+			result.put(c.getId(), c.getName());
+		}
+		
+		return result;
+	}
+	
+	public String getClient(String id) {
+		
+		Client c = clients.get(id);
+		
+		if (c == null) {
+			
+			return null;
+		}
+		
+		return "ID: " + c.getId() + " Name: " + c.getName() + " Email: " + c.getEmail();
+	}
+
+	
 }
