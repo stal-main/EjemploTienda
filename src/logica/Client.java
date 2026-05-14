ad4a278184db985323bd09693038c20831f7de1e
Kevin Solano <7stalyakov7@gmail.com>
2026-05-14 00:23:57 -0600

    Clase client terminada

--- src/logica/Client.java
+++ src/logica/Client.java
@@ -1,5 +1,6 @@
 package logica;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Client {
@@ -10,6 +11,8 @@ public class Client {
 	
 	private String email;
 	
+	private Map<Integer, Order> orders;
+	
 	public Client(String id, String name, String email) {
 		
 		this.id = id;
@@ -17,6 +20,8 @@ public class Client {
 		this.name = name;
 		
 		this.email = email;
+		
+		this.orders = new HashMap<>();
 	}
 	
 	public String getId() {
@@ -36,26 +41,59 @@ public class Client {
 	
 	public void updateClient(String name, String email) {
 		
+		this.name = name;
+		
+		this.email = email;
 	}
 	
-	public double RemainingTotal() {
+	public double getTotalPending() {
 		
+		double total = 0;
 		
+		for (Order o : orders.values()) {
+			
+			if ("Pending".equalsIgnoreCase(o.getStatus())) {
+				
+				total += o.getTotalAmout();
+			}
+		}
+		
+		return total;
 	}
 	
 	public Map<Integer, String> getOrders() {
 		
+		Map<Integer, String> result = new HashMap<>();
+		
+		for (Order o : orders.values()) {
+			
+			result.put(o.getNumber(), o.getStatus());
+		}
+		
+		return result;
 	}
 	
 	public Map<Integer, String> getOrdersByStatus(String status) {
+		Map<Integer, String> result = new HashMap<>();
 		
+		for (Order o : orders.values()) {
+			
+			if (o.getStatus().equalsIgnoreCase(status)) {
+				
+				result.put(o.getNumber(), o.getStatus());
+			}
+		}
+		
+		return result;
 	}
 	
-	public void addOrder() {
-	
+	public void addOrder(Order order) {
+		
+		orders.put(order.getNumber(), order);
 	}
 	
 	public void deleteOrder(int number) {
 		
+		orders.remove(number);
 	}
 }
